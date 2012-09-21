23b3238c6a91761e280d3f4015c03a8aa04aeaab
harry <harry@redant.com.au>
2012-09-21 15:27:56 +1000

    add book case

---
+++ 10-book-case/ruby1.9/samples/appsocket_2.rb
@@ -0,0 +1,16 @@
+#---
+# Excerpted from "Programming Ruby",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/ruby3 for more book information.
+#---
+#        require 'socket'
+#          for line in Socket.getaddrinfo('www.microsoft.com', 'http')
+#            puts line.join(", ")
+#          end
+ puts 'AF_INET, 80, wwwbaytest1.microsoft.com, 207.46.19.190, 2, 2, 17'
+ puts 'AF_INET, 80, wwwbaytest1.microsoft.com, 207.46.19.190, 2, 1, 6'
+ puts 'AF_INET, 80, wwwbaytest2.microsoft.com, 207.46.19.254, 2, 2, 17'
+ puts 'AF_INET, 80, wwwbaytest2.microsoft.com, 207.46.19.254, 2, 1, 6' 
